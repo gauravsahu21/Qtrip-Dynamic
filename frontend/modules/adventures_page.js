f414e4b9c1645523045559645a3c0219678db5c3
Kiran Suresh <kiran@criodo.com>
2020-11-11 00:31:52 +0550

    updated changes - 2020-11-11 00:11:52

--- frontend/modules/adventures_page.js
+++ frontend/modules/adventures_page.js
@@ -3,81 +3,37 @@ import config from "../conf/index.js";
 
 //Implementation to extract city from query params
 function getCityFromURL(search) {
-  const urlParams = new URLSearchParams(search);
-  const city = urlParams.get("city");
-  return city;
+  // TODO: MODULE_ADVENTURES
+  // 1. Extract the city id from the URL's Query Param and return it
+
 }
 
 //Implementation of fetch call with a paramterized input based on city
 async function fetchAdventures(city) {
-  try {
-    const result = await fetch(
-      config.backendEndpoint + `/adventures?city=${city}`
-    );
-    const data = await result.json();
-    return data;
-  } catch (e) {
-    return null;
-  }
+  // TODO: MODULE_ADVENTURES
+  // 1. Fetch adventures using the Backend API and return the data
+
 }
+
 //Implementation of DOM manipulation to add adventures for the given city from list of adventures
 function addAdventureToDOM(adventures) {
-  adventures.forEach((key) => {
-    let ele = document.createElement("div");
-    ele.className = "col-6 col-lg-3 mb-4";
-    ele.innerHTML = `
-            <a href="detail/?adventure=${key.id}" id=${key.id}>
-            <div class="category-banner">${key.category}</div>
-              <div class="activity-card">
+  // TODO: MODULE_ADVENTURES
+  // 1. Populate the Adventure Cards and insert those details into the DOM
 
-                <img
-                  class="img-responsive"
-                  src=${key.image}
-                />
-
-                <div class="activity-card-text text-md-center w-100 mt-3">
-                  <div class="d-block d-md-flex justify-content-between flex-wrap pl-3 pr-3">
-                    <h5 class="text-left">${key.name}</h5>
-                    <p>₹${key.costPerHead}</p>
-                  </div>
-                    <div class="d-block d-md-flex justify-content-between flex-wrap pl-3 pr-3">
-                    <h5 class="text-left">Duration</h5>
-                    <p>${key.duration} Hours</p>
-                  </div>
-                </div>
-              </div>
-            </a>
-          `;
-
-    document.getElementById("data").appendChild(ele);
-  });
 }
 
 //Implementation of filtering by duration which takes in a list of adventures, the lower bound and upper bound of duration and returns a filtered list of adventures.
 function filterByDuration(list, low, high) {
-  let filteredList = [];
+  // TODO: MODULE_FILTERS
+  // 1. Filter adventures based on Duration and return filtered list
 
-  list.map((key) => {
-    if (key.duration > low && key.duration <= high) {
-      filteredList.push(key);
-    }
-  });
-
-  return filteredList;
 }
 
 //Implementation of filtering by category which takes in a list of adventures, list of categories to be filtered upon and returns a filtered list of adventures.
 function filterByCategory(list, categoryList) {
-  let filteredList = [];
-  categoryList.map((category) => {
-    list.map((key) => {
-      if (key.category === category) {
-        filteredList.push(key);
-      }
-    });
-  });
+  // TODO: MODULE_FILTERS
+  // 1. Filter adventures based on their Category and return filtered list
 
-  return filteredList;
 }
 
 // filters object looks like this filters = { duration: "", category: [] };
@@ -88,50 +44,31 @@ function filterByCategory(list, categoryList) {
 // 3. Filter by duration and category together
 
 function filterFunction(list, filters) {
-  let filteredList = [];
+  // TODO: MODULE_FILTERS
+  // 1. Handle the 3 cases detailed in the comments above and return the filtered list of adventures
+  // 2. Depending on which filters are needed, invoke the filterByDuration() and/or filterByCategory() methods
 
-  // 3. Filter by duration and category together
-  if (filters["duration"].length > 0 && filters["category"].length > 0) {
-    let choice = filters["duration"].split("-");
-    filteredList = filterByDuration(
-      list,
-      parseInt(choice[0]),
-      parseInt(choice[1])
-    );
-    filteredList = filterByCategory(filteredList, filters["category"]);
-  }
 
-  // 2. Filter by duration only
-  else if (filters["duration"].length > 0) {
-    let choice = filters["duration"].split("-");
-    filteredList = filterByDuration(
-      list,
-      parseInt(choice[0]),
-      parseInt(choice[1])
-    );
-  }
-
-  // 1. Filter by category only
-  else if (filters["category"].length > 0) {
-    filteredList = filterByCategory(list, filters["category"]);
-  }
-
-  // default case when there is no filter
-  else {
-    filteredList = list;
-  }
-  return filteredList;
+  // Place holder for functionality to work in the Stubs
+  return list;
 }
 
 //Implementation of localStorage API to save filters to local storage. This should get called everytime an onChange() happens in either of filter dropdowns
 function saveFiltersToLocalStorage(filters) {
-  localStorage.setItem("filters", JSON.stringify(filters));
+  // TODO: MODULE_FILTERS
+  // 1. Store the filters to localStorage using JSON.stringify()
+
   return true;
 }
 
 //Implementation of localStorage API to get filters from local storage. This should get called whenever the DOM is loaded.
 function getFiltersFromLocalStorage() {
-  return JSON.parse(localStorage.getItem("filters"));
+  // TODO: MODULE_FILTERS
+  // 1. Get the filters from localStorage and return in JSON format
+
+
+  // Place holder for functionality to work in the Stubs
+  return null;
 }
 
 //Implementation of DOM manipulation to add the following filters to DOM :
@@ -139,19 +76,9 @@ function getFiltersFromLocalStorage() {
 // 2. Update the category pills on the DOM
 
 function generateFilterPillsAndUpdateDOM(filters) {
-  //Sets value in dropdown for duration filter
-  document.getElementById("duration-select").value = filters.duration;
+  // TODO: MODULE_FILTERS
+  // 1. Use the filters given as input, update the Duration Filter and Generate Category Pills
 
-  //Iterates over category filters and inserts category pills into DOM
-  filters["category"].map((key) => {
-    let ele = document.createElement("div");
-    ele.className = "category-filter";
-    ele.innerHTML = `
-                 <div>${key}</div>
-                `;
-
-    document.getElementById("category-list").appendChild(ele);
-  });
 }
 export {
   getCityFromURL,
--- frontend/modules/reservation_page.js
+++ frontend/modules/reservation_page.js
@@ -2,26 +2,22 @@ import config from "../conf/index.js";
 
 //Implementation of fetch call to fetch all reservations
 async function fetchReservations() {
-  try {
-    const result = await fetch(config.backendEndpoint + `/reservations/`);
-    const data = await result.json();
-    return data;
-  } catch (e) {
-    return null;
-  }
+  // TODO: MODULE_RESERVATIONS
+  // 1. Fetch Reservations by invoking the REST API and return them
+
+
+  // Place holder for functionality to work in the Stubs
+  return null;
 }
 
 //Function to add reservations to the table. Also; in case of no reservations, display the no-reservation-banner, else hide it.
 function addReservationToTable(reservations) {
-  //Conditionally render the no-reservation-banner and reservation-table-parent
-  if (reservations.length > 0) {
-    document.getElementById("no-reservation-banner").style.display = "none";
-    document.getElementById("reservation-table-parent").style.display = "block";
-  } else {
-    document.getElementById("no-reservation-banner").style.display = "block";
-    document.getElementById("reservation-table-parent").style.display = "none";
-  }
+  // TODO: MODULE_RESERVATIONS
+  // 1. Add the Reservations to the HTML DOM so that they show up in the table
 
+  
+  //Conditionally render the no-reservation-banner and reservation-table-parent
+  
   /*
     Iterating over reservations, adding it to table (into div with class "reservation-table") and link it correctly to respective adventure
     The last column of the table should have a "Visit Adventure" button with id=<reservation-id>, class=reservation-visit-button and should link to respective adventure page
@@ -31,34 +27,6 @@ function addReservationToTable(reservations) {
     2. The booking time should appear in a format like 4 November 2020, 9:32:31 pm
   */
 
-  reservations.map((key, idx) => {
-    let ele = document.createElement("tr");
-    ele.innerHTML = `
-          <th scope="row" >${key.id}</th>
-          <td>${key.name}</td>
-          <td>${key.adventureName}</td>
-          <td>${key.person}</td>
-          <td>${new Date(key.date).toLocaleDateString("en-IN")}</td>
-           <td>${key.price}</td>
-           <td>${new Date(key.time).toLocaleString("en-IN", {
-             year: "numeric",
-             day: "numeric",
-             month: "long",
-             hour: "numeric",
-             minute: "numeric",
-             second: "numeric",
-             hour12: true,
-           })}</td>
-           <td><div class="reservation-visit-button" id=${
-             key.id
-           }><a href="/frontend/pages/adventures/detail/?adventure=${
-      key.adventure
-    }">Visit Adventure</a></div></td>
-
-          `;
-
-    document.getElementById("reservation-table").appendChild(ele);
-  });
 }
 
 export { fetchReservations, addReservationToTable };
