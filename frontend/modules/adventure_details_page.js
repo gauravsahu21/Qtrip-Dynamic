4b2e22ad9eb5433f1d7befca62e2b8cdfef1b76c
Kiran Suresh <kiran@criodo.com>
2020-11-12 20:57:04 +0550

    updated changes - 2020-11-12 20:37:04

---
+++ frontend/modules/adventure_details_page.js
@@ -0,0 +1,73 @@
+import config from "../conf/index.js";
+
+//Implementation to extract adventure ID from query params
+function getAdventureIdFromURL(search) {
+  // TODO: MODULE_ADVENTURE_DETAILS
+  // 1. 
+
+
+  // Place holder for functionality to work in the Stubs
+  return null;
+}
+//Implementation of fetch call with a paramterized input based on adventure ID
+async function fetchAdventureDetails(adventureId) {
+  // TODO: MODULE_ADVENTURE_DETAILS
+  // 1. 
+
+
+  // Place holder for functionality to work in the Stubs
+  return null;
+}
+
+//Implementation of DOM manipulation to add adventure details to DOM
+function addAdventureDetailsToDOM(adventure) {
+  // TODO: MODULE_ADVENTURE_DETAILS
+  // 1. 
+
+}
+
+//Implementation of conditional rendering of DOM based on availability
+function conditionalRenderingOfReservationPanel(adventure) {
+  // TODO: MODULE_ADVENTURE_DETAILS
+  // 1. 
+
+}
+
+//Implementation of bootstrap gallery component
+function addBootstrapPhotoGallery(images) {
+  // TODO: MODULE_ADVENTURE_DETAILS
+  // 1. 
+
+}
+
+//Implementation of reservation cost calculation based on persons
+function calculateReservationCostAndUpdateDOM(adventure, persons) {
+  // TODO: MODULE_ADVENTURE_DETAILS
+  // 1. 
+
+}
+
+//Implementation of reservation form submission using JQuery
+function captureFormSubmitUsingJQuery(adventure) {
+  // TODO: MODULE_ADVENTURE_DETAILS
+  // 1. 
+
+}
+
+//Implementation of success banner after reservation
+function showBannerIfAlreadyReserved(adventure) {
+  // TODO: MODULE_ADVENTURE_DETAILS
+  // 1. 
+
+}
+
+export {
+  getAdventureIdFromURL,
+  fetchAdventureDetails,
+  addAdventureDetailsToDOM,
+  addBootstrapPhotoGallery,
+  conditionalRenderingOfReservationPanel,
+  captureFormSubmitUsingJQuery,
+  calculateReservationCostAndUpdateDOM,
+  showBannerIfAlreadyReserved,
+};
